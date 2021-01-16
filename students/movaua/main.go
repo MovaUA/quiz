4341af3a18bb1a3ea3320ffd46b8b4d2cb7ef80b
mova <valeriy.molchanov.77@gmail.com>
2021-01-16 09:46:49 +0200

    quiz part 2 timer

--- students/movaua/main.go
+++ students/movaua/main.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"encoding/csv"
 	"flag"
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	filename := flag.String("file", "problems.csv", "CSV file with problems to solve")
+	seconds := flag.Uint("time", 30, "timeout (seconds) for solving all problems")
+
 	flag.Parse()
+
+	fmt.Println("press Enter to start...")
+
+	r := bufio.NewReader(os.Stdin)
+
+	if _, err := r.ReadString('\n'); err != nil {
+		fmt.Printf("\ncould not read stdin: %v\n", err)
+		os.Exit(1)
+	}
 
 	fmt.Println("Solve the problems:")
 
@@ -30,22 +43,49 @@ func main() {
 		os.Exit(1)
 	}
 
-	inReader := bufio.NewReader(os.Stdin)
 	correct := 0
 
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		time.Duration(*seconds)*time.Second,
+	)
+	defer cancel()
+
+	answerCh := make(chan a)
+	go readAnswer(r, answerCh)
+
+loop:
 	for _, record := range records {
 		fmt.Printf("%v = ", record[0])
 
-		line, err := inReader.ReadString('\n')
-		if err != nil {
-			fmt.Printf("could not read answer: %v\n", err)
-		}
-
-		answer := strings.TrimSpace(line)
-		if answer == record[1] {
-			correct++
+		select {
+		case a := <-answerCh:
+			if a.err != nil {
+				fmt.Printf("could not read answer: %v\n", err)
+				os.Exit(1)
+			}
+			if strings.EqualFold(a.answer, record[1]) {
+				correct++
+			}
+		case <-ctx.Done():
+			fmt.Printf("\ntimeout of %d seconds is expired\n", *seconds)
+			break loop
 		}
 	}
 
 	fmt.Printf("Your score is %d of %d\n", correct, len(records))
+}
+
+func readAnswer(r *bufio.Reader, answerCh chan<- a) {
+	line, err := r.ReadString('\n')
+	if err != nil {
+		answerCh <- a{err: err}
+		return
+	}
+	answerCh <- a{answer: strings.TrimSpace(line)}
+}
+
+type a struct {
+	answer string
+	err    error
 }
